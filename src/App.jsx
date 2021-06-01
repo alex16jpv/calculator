64ea5cfa2ba5f2ebacb228b1507ea16344be517d
Alexander P. Valencia <alex16jpv@gmail.com>
2021-05-31 23:56:39 -0500

    Add click feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,49 @@
+import { useState, useEffect } from "react";
 import Button from "./components/Button";
 import "./App.css";
 
 function App() {
+  const [stack, setStack] = useState("0");
+
+  const clickHandler = (text) => {
+    setStack(text);
+  };
+
+  useEffect(() => {
+    console.log(`Stack: ${stack}`);
+  }, [stack]);
+
   return (
     <main className="calculator">
-      <section className="calculator__display">0</section>
+      <section className="calculator__display">{stack}</section>
       <section className="calculator__keypad">
         <div className="calculator__input-keys">
           <div className="calculator__function-keys">
-            <Button text="C" />
-            <Button text="+/-" />
-            <Button text="%" />
+            <Button text="C" clickHandler={clickHandler} />
+            <Button text="+/-" clickHandler={clickHandler} />
+            <Button text="%" clickHandler={clickHandler} />
           </div>
           <div className="calculator__digit-keys">
-            <Button text="7" />
-            <Button text="8" />
-            <Button text="9" />
-            <Button text="4" />
-            <Button text="5" />
-            <Button text="6" />
-            <Button text="1" />
-            <Button text="2" />
-            <Button text="3" />
-            <Button text="00" />
-            <Button text="0" />
-            <Button text="." />
+            <Button text="7" clickHandler={clickHandler} />
+            <Button text="8" clickHandler={clickHandler} />
+            <Button text="9" clickHandler={clickHandler} />
+            <Button text="4" clickHandler={clickHandler} />
+            <Button text="5" clickHandler={clickHandler} />
+            <Button text="6" clickHandler={clickHandler} />
+            <Button text="1" clickHandler={clickHandler} />
+            <Button text="2" clickHandler={clickHandler} />
+            <Button text="3" clickHandler={clickHandler} />
+            <Button text="00" clickHandler={clickHandler} />
+            <Button text="0" clickHandler={clickHandler} />
+            <Button text="." clickHandler={clickHandler} />
           </div>
         </div>
         <div className="calculator__operator-keys">
-          <Button text="/" />
-          <Button text="*" />
-          <Button text="-" />
-          <Button text="+" />
-          <Button text="=" />
+          <Button text="/" clickHandler={clickHandler} />
+          <Button text="*" clickHandler={clickHandler} />
+          <Button text="-" clickHandler={clickHandler} />
+          <Button text="+" clickHandler={clickHandler} />
+          <Button text="=" clickHandler={clickHandler} />
         </div>
       </section>
     </main>
