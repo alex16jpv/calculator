b4b53860bb36a242972633089687905e4506c4f1
Alexander P. Valencia <alex16jpv@gmail.com>
2021-05-31 23:46:34 -0500

    initial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Calculator</div>;
+  return (
+    <main className="calculator">
+      <section className="calculator__display">0</section>
+      <section className="calculator__keypad">
+        <div className="calculator__input-keys">
+          <div className="calculator__function-keys">
+            <button className="calculator-key">C</button>
+            <button className="calculator-key">+/-</button>
+            <button className="calculator-key">%</button>
+          </div>
+          <div className="calculator__digit-keys">
+            <button className="calculator-key">7</button>
+            <button className="calculator-key">8</button>
+            <button className="calculator-key">9</button>
+            <button className="calculator-key">4</button>
+            <button className="calculator-key">5</button>
+            <button className="calculator-key">6</button>
+            <button className="calculator-key">1</button>
+            <button className="calculator-key">2</button>
+            <button className="calculator-key">3</button>
+            <button className="calculator-key">00</button>
+            <button className="calculator-key">0</button>
+            <button className="calculator-key">.</button>
+          </div>
+        </div>
+        <div className="calculator__operator-keys">
+          <button className="calculator-key">/</button>
+          <button className="calculator-key">*</button>
+          <button className="calculator-key">-</button>
+          <button className="calculator-key">+</button>
+          <button className="calculator-key">=</button>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
